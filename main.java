243bc2cb870bdfba66b3b73563ad6a436ea149a1
Dom.SEngineer <dominic.allen998@gmail.com>
2025-01-30 09:57:46 -0800

    added Account class

--- main.java
+++ main.java
@@ -1,4 +1,9 @@
+
 class BankingSystem{
+    public class Account {
+        String name;
+        double balance;
+    }
     public static void main(String[] args)
     {
         System.out.println("Hello World");
